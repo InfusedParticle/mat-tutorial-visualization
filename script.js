570aa688c16b459d110d8f687edca7e7dd6dcc90
InfusedParticle <74432164+InfusedParticle@users.noreply.github.com>
2023-09-18 22:09:34 -0500

    optimized mobile, starting to work on modals

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ function init() {
     let selectors = document.querySelectorAll('select');
     var instances = M.FormSelect.init(selectors);
 
+    let modals = document.querySelectorAll('.modal');
+    var instances2 = M.Modal.init(modals);
+
     let difficultyFilter = document.getElementById('difficulties')
     difficultyFilter.addEventListener('change', ()=>{
         let instance = M.FormSelect.getInstance(document.getElementById('difficulties'))
@@ -50,65 +53,75 @@ function init() {
             console.log(row)
             if(row[5]['v'].includes('specific')) {
                 // one-time tutorials
-                let d = document.createElement('div')
-                d.className = "card-panel blue lighten-2 tutorialslot"
-                let course = document.createElement('div')
-                let courseName = document.createElement('div')
-                let teacher = document.createElement('div')
-                courseName.innerHTML = row[3].v;
-                teacher.innerHTML = `<b>${row[2].v}</b>`;
-                teacher.classList.add('teacher')
-                course.appendChild(courseName)
-                course.appendChild(teacher)
-            
-                course.classList.add("course")
-            
-                let time = document.createElement("div")
-                let tempTime = row[11]['f']
-                time.innerHTML = tempTime.substring(0, tempTime.lastIndexOf(':')) + tempTime.substring(tempTime.lastIndexOf(' '))
-                time.classList.add('time')
-
-                let date = document.createElement("div")
-
-                date.innerHTML = row[10]['f']
-                d.appendChild(course);
-                d.appendChild(date)
-                d.appendChild(time);
-                d.style.padding = "10px 15px 10px 15px";
-        
-                let tutorials = document.getElementById("onetime")
-                tutorials.appendChild(d)
+                oneTimeTutorial(row)
             }
             else {
                 // reoccurring tutorial
-                let d = document.createElement('div')
-                d.className = "card-panel blue lighten-2 tutorialslot"
-                let course = document.createElement('div')
-                let courseName = document.createElement('div')
-                let teacher = document.createElement('div')
-                courseName.innerHTML = row[3].v;
-                teacher.innerHTML = `<b>${row[2].v}</b>`;
-                teacher.classList.add('teacher')
-                course.appendChild(courseName)
-                course.appendChild(teacher)
-            
-                course.classList.add("course")
-            
-                let time = document.createElement("div")
-                time.innerHTML = row[7]['v']
-                time.classList.add('time')
-                let days = document.createElement("div")
-                days.innerHTML = row[6]['v']
-
-                d.appendChild(course);
-                d.appendChild(days)
-                d.appendChild(time);
-                d.style.padding = "10px 15px 10px 15px";
-        
-                let tutorials = document.getElementById("recurring")
-                tutorials.appendChild(d)
+                recurringTutorial(row)
             }
         }
         
     })
+}
+
+function oneTimeTutorial(row) {
+    let d = document.createElement('div')
+    d.className = "card-panel blue lighten-2 tutorialslot"
+    let course = document.createElement('div')
+    let courseName = document.createElement('div')
+    let teacher = document.createElement('div')
+    courseName.innerHTML = row[3].v;
+    teacher.innerHTML = `<b>${row[2].v} - ${row[4].v}</b>`;
+    teacher.classList.add('teacher')
+    course.appendChild(courseName)
+    course.appendChild(teacher)
+    
+    course.classList.add("course")
+    
+    let time = document.createElement("div")
+    let tempTime = row[11]['f']
+    time.innerHTML = tempTime.substring(0, tempTime.lastIndexOf(':')) + tempTime.substring(tempTime.lastIndexOf(' '))
+    time.classList.add('time')
+
+    let date = document.createElement("div")
+
+    date.innerHTML = row[10]['f']
+    d.appendChild(course);
+    d.appendChild(date)
+    d.appendChild(time);
+    d.style.padding = "10px 15px 10px 15px";
+    
+    let tutorials = document.getElementById("onetime")
+    tutorials.appendChild(d)
+
+    
+}
+
+function recurringTutorial(row) {
+    let d = document.createElement('div')
+    d.className = "card-panel blue lighten-2 tutorialslot"
+    let course = document.createElement('div')
+    let courseName = document.createElement('div')
+    let teacher = document.createElement('div')
+    courseName.innerHTML = row[3].v;
+    teacher.innerHTML = `<b>${row[2].v} - ${row[4].v}</b>`;
+    teacher.classList.add('teacher')
+    course.appendChild(courseName)
+    course.appendChild(teacher)
+            
+    course.classList.add("course")
+            
+    let time = document.createElement("div")
+    time.innerHTML = row[7]['v']
+    time.classList.add('time')
+    let days = document.createElement("div")
+    days.innerHTML = row[6]['v']
+
+    d.appendChild(course);
+    d.appendChild(days)
+    d.appendChild(time);
+    d.style.padding = "10px 15px 10px 15px";
+        
+    let tutorials = document.getElementById("recurring")
+    tutorials.appendChild(d)
 }
